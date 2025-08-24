eb18381734ead07a0848f92c71878b058c2c92fc
Malsoryz <ikmalalansory@gmail.com>
2025-08-24 14:00:05 +0800

    berasal dari sumber terpercaya

--- source/Malsoryz/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <main className="min-h-screen w-full grid justify-center items-center">
-      <div className="title">ALTERNITY</div>
-    </main>
-  )
-}
